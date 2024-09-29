59a1ef701bde79328137b4c5632becf866229dee
Yuki <rategyst@gmail.com>
2024-09-29 08:29:33 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,17 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: %i[new create edit update]
   # ruby practice that interface to Microposts resoirces via profile n Home page controller
   # so microposts's new n edit are no need
   resources :microposts,          only: %i[create destroy]
+  resources :relationships,       only: %i[create destroy]
   get '/microposts', to: 'static_pages#home'
 end
